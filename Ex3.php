6d45f8becc2b5fb3d0827e86bcce8ceeb82f5e21
suwanun2537 <6216013805013@pngcc.ac.th>
2021-06-26 13:00:06 +0700

    Add file Ex3.php Ex4.php Ex5.php

---
+++ Ex3.php
@@ -0,0 +1,13 @@
+<?php
+ $number1 = 10;
+ $number2 = 20;
+
+ echo $number1;
+ echo $number1;
+ echo "<br>";
+ echo ("number1 = " . $number1 ."number2 = ". $number2 . "<br>") ;
+
+ echo ($number1 ." + ". $number2 ." = ". ($number1+$number2)) ;
+
+ 
+?>
---
+++ Ex5.php
@@ -0,0 +1,18 @@
+<?php
+//โปรแกรมคำนวณราคาสินค้า
+ $price = 20; //ราคาสินค้า
+ $quantiry = 30; //จำนวน
+ $total = $price * $quantiry; // ราคารวมคิดโดยนำ ราคา x จำนวน
+
+ //ภาษี   7%
+ $vat = $total * 0.07;
+ $totalall = $total + $vat;
+
+ echo ("ราคาสินค้า  " . $price . "จำนวน " .$quantiry. "ราคารวม" .$total );
+ echo (" <br>");
+ echo ("ภาษี 7%  = " .$vat  );
+ echo (" <br>");
+ echo ("ราคาสินค้ารวมภาษี =  " . $totalall . "บาท "  );
+
+
+?>
